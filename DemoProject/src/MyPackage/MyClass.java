866d97d88cca044777dc9dd6117af5eeaac696dd
vicky08101997 <vikram.kekan66@gmail.com>
2023-01-29 20:35:36 +0530

    abcde

---
+++ DemoProject/src/MyPackage/MyClass.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+public class MyClass {
+public static void main(String[] args) {
+	System.out.println("a");
+	System.out.println("b");
+	System.out.println("c");
+	System.out.println("d");
+	System.out.println("e");
+}
+}
